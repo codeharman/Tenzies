801515d1d9916086469e134c55411665d1b7d2fb
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2026-01-11 16:18:45 +0530

    roll button made that can make random numbers each time

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Die from './Die.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [dice, setDice] = useState(randomNumber())
+
+  function randomNumber(){
+    let arr = []
+    for(let i = 0; i < 10; i++){
+      let random = Math.ceil(Math.random() * 6)
+      arr.push(random)
+    }
+    return arr
+  }
+
+  function result(){
+    setDice(
+      randomNumber()
+    )
+  }
+
+  const diceResult = dice.map(data => <Die value={data}/>)
+  const button = <button onClick={result}>Roll Dice</button>
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className='main-container'>
+        {diceResult}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {button}
+    </main>
   )
 }
 
